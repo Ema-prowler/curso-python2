e2528d7be6d593e34f6db88439a970fc2d12e063
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-10-28 20:59:24 -0300

    Asignacion multiple

---
+++ asignacionMultiple.py
@@ -0,0 +1,23 @@
+# nombre = "ema"  # asignacion de una variable
+# edad = 26  # asignacion de una variable
+# atractivo = True  # asignacion de una variable
+
+nombre, edad, atractivo = "ema", 26, True  # asignacion de varias variables
+
+print(nombre)
+print(edad)
+print(atractivo)
+
+#nombre1 = 10
+#nombre2 = 10
+#nombre3 = 10
+#nombre4 = 10
+
+nombre1 = nombre2 = nombre3 = nombre4 = 10  # asignacion de varias variables
+
+print(nombre1)
+print(nombre2)
+print(nombre3)
+print(nombre4)
+
+
