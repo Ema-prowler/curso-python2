86a6694c38dc610511221f8d2e5889430d54222c
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-10-28 21:31:40 -0300

    Metodos para cadenas de texto

---
+++ metodosParaCadenasDeTexto.py
@@ -0,0 +1,14 @@
+nombre = "Emanuel code "
+
+# print(len(nombre))
+
+# print(nombre.find("a"))  # devuelve la posicion de la primera aparicion de la letra 'a' en el nombre
+# print(nombre.capitalize()) # devuelve la primera letra en mayusculas
+# print(nombre.upper())  # devuelve toda la cadena en mayusculas
+# print(nombre.lower())  # devuelve toda la cadena en minusculas
+# print(nombre.isdigit())  # devuelve True si la cadena contiene solo numeros
+# print(nombre.isalpha())  # devuelve True si la cadena contiene solo letras
+# print(nombre.count("e"))  # devuelve el numero de veces que la cadena contiene la letra 'e'
+# print(nombre.replace("c", "d"))  # devuelve una nueva cadena con la letra 'd' en lugar de la letra 'c'
+# print(nombre*4)  # devuelve una nueva cadena con 4 veces la cadena original
+
