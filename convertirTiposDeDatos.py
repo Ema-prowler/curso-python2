b89610550ff955fa40ec4ffbaf13f7ed26f1b013
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-10-29 16:06:16 -0300

    Convertir tipos de datos

---
+++ convertirTiposDeDatos.py
@@ -0,0 +1,33 @@
+# Conversion de tipos de datos
+
+
+x = 1
+y = 2.0
+z = "3"
+
+print(x)    # int
+print(y)    # float
+print(z)    # str
+print("separador")
+# Para convertir un numero a una cadena o un entero o un float
+# Utilizamos la funcion str()
+# Tambien podemos utilizar la funcion int() o float() para sus respectivos tipos.
+
+
+print(str(y))
+print(int(y))
+print("separador")
+
+# para asignar y convertir un tipo de datos a otro:
+
+x = float(x)
+print(x)
+
+y = int(y)
+print(y)
+
+# Ejemplo de uso
+
+edad = 24
+
+print("Eda: " + str(edad)) # En una cadena de texto concatenada
