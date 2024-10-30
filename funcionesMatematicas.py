36dcef3aeb8b435354c2f05fecbcb4bf98f865df
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-10-29 21:28:41 -0300

    Funciones Matematicas

---
+++ funcionesMatematicas.py
@@ -0,0 +1,23 @@
+# Funciones matematicas
+
+import math
+
+PI = 3.14   # Constante matematica
+piNegativo = -3.14
+
+x = 2
+y = 1
+z = 3
+
+print(round(PI))    # devuelve el valor de PI redondeado a un numero entero
+print(math.ceil(PI))    # devuelve el valor de PI redondeado hacia arriba
+print(math.floor(PI))    # devuelve el valor de PI redondeado hacia abajo
+print(abs(piNegativo))    # devuelve el valor absoluto de PI
+print(pow(PI, 2))    # devuelve la potencia del exponente 2 de PI en este caso
+print(math.sqrt(420))    # devuelve la raíz cuadrada de un numero dado
+
+# Estas 2 ultimas son muy utiles con listas y tuplas
+print(max(x, y, z))    # devuelve el valor maximo de los tres numeros dados
+print(min(x, y, z))    # devuelve el valor minimo de los tres numeros dados
+
+
