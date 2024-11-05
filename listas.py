11b4a51ce1a0a249aefd8646e37aff0fc60a1c7b
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-11-05 16:17:56 -0300

    Listas

---
+++ listas.py
@@ -0,0 +1,23 @@
+# listas
+# una lista se usa para almacenar multiples elemntos dentro de una sola varibale.
+
+comida = ["Pizza", "Hamburguesa", "Hot Dog", "Espaguetis", "Pudding" , 1, 2.5, True]
+
+comida[0] = "Helado"
+
+# append("Sushi") # agrega un elemento al final de la lista
+# comida.remove("Hamburguesa") # borra un elemento de la lista
+# comida.pop()  # borra el ultimo elemento de la lista
+# comida.insert(0, "Pastel")  # inserta un elemento en el indice indicado
+# comida.sort()  # ordena la lista alfabeticamente
+# comida.clear()  # borra toda la lista
+
+for i in comida:
+    print(i)
+
+
+# print(comida , " Lista completa")  # imprime el contenido de la lista
+# print(comida[0] + " |imprime solo el elemento 0 de la lista")  # imprime el primer elemento de la lista
+# Se puede actualizar el contenido de una lista despues de declararla.
+
+
