a45334e5ab5b533d2cb62bfcedd48207295374b9
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-11-11 14:19:15 -0300

    Funciones anidadas

---
+++ funciones_anidadas.py
@@ -0,0 +1,14 @@
+# funciones anidadas
+
+# Forma mas larga
+
+# num = input("Ingresa un numero: ") # Ingresa un numero
+# num = float(num) # Convierte el numero a un float
+# num = abs(num) # Obtiene el valor absoluto
+# num = round(num) # Convierte el numero a un entero
+# print(num) # Imprime el numero
+
+# forma mas corta
+
+print((round(abs(float(input("ingresa un numero: ")))))) # hace todas las operaciones en una sola linea.
+
