b482c6f2da024acce655fcbe18688b2961f49df4
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-11-11 10:45:34 -0300

    Declaracion return

---
+++ return.py
@@ -0,0 +1,14 @@
+# Declaracion return
+
+# En Python, la declaración return se usa en una función para devolver un valor al
+# lugar desde donde fue llamada.
+
+def multiplicar(numero_uno, numero_dos):
+    return numero_uno * numero_dos
+
+# print(multiplicar(6, 8)) # una manera de imprimir el resultado
+#--------------------------
+# Otra manera de imprimir el resultado
+x = multiplicar(6, 8)
+print(x)
+#--------------------------
