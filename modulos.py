0ed3292fae9135eec181df17ca90c34e69fdc5d1
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-11-14 15:13:18 -0300

    Modulos

---
+++ modulos.py
@@ -0,0 +1,16 @@
+# Modulos
+
+#import mensaje as msj                 # importar el modulo mensaje
+# se le puede poner un alias al modulo en este caso msj
+#from mensaje import hola, adios       # importar las funciones hola y adios del modulo mensaje
+# de esta forma ya no es necesario escribir msj o el nombre del modulo.
+
+# otra forma de importar modulos es:
+#from mensaje import *                  # importar todas las funciones del modulo mensaje
+
+
+
+#hola()
+#adios()
+
+help("modules") # imprime todos los modulos disponibles que posee python
