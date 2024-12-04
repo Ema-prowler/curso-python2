4f339d6be7b2107477fd7f0f3bc74e6804cad368
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-12-04 20:17:08 -0300

    Asignar una funcion a una variable

---
+++ asignar_una_funcion_a_una_variable.py
@@ -0,0 +1,13 @@
+# Asignar una funcion a una variable
+
+def hola():
+    print("Hola")
+
+#hola() # los parentesis son los que se usan para llamar a una funcion
+
+h = hola
+
+h()
+
+decir = print
+decir("Sorprendente")
