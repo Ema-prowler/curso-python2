263d14e8b1bba99c84e832e5664bb25a56fe564a
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-12-10 21:23:44 -0300

    Funcion map

---
+++ funcion_map.py
@@ -0,0 +1,20 @@
+# Funciones map
+
+tienda = [("Camisa", 20.00),
+          ("Pantalones", 25.00),
+          ("Chaqueta", 50.00),
+          ("Medias", 10.000)]
+
+fun_euro = lambda datos: (datos[0], datos[1]*0.96)
+fun_dolar = lambda datos: (datos[0], datos[1]/0.96)
+
+#tienda2 = list(map(fun_euro, tienda))
+tienda2 = list(map(fun_dolar, tienda))
+
+
+print(type(tienda2))
+
+
+for i in tienda2:
+    print(i)
+
