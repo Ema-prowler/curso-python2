cc708a16dc859307c7c75e3f9593490c4709a0e3
Emanuel Coletti <emanuelcoletti001@gmail.com>
2025-01-09 19:38:32 -0300

    Eventos de mouse con tkinter

---
+++ eventos_mouse_tkinter.py
@@ -0,0 +1,20 @@
+# Eventos de mouse
+
+from tkinter import *
+
+
+def hacer_algo(evento):
+    print('Coordenadas: ' + str(evento.x) + ', ' + str(evento.y))
+
+window = Tk()
+
+
+#window.bind("<Button-1>",hacer_algo)
+#window.bind("<Button-2>",hacer_algo)
+#window.bind("<Button-3>",hacer_algo)
+#window.bind("<ButtonRelease>",hacer_algo)
+#window.bind("<Enter>",hacer_algo)
+#window.bind("<Leave>",hacer_algo)
+window.bind("<Motion>",hacer_algo)
+
+window.mainloop()
