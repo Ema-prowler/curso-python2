1538b8219b02bfeffddec54d1c3139911e316d36
Emanuel Coletti <emanuelcoletti001@gmail.com>
2025-01-15 16:37:16 -0300

    Reloj con Tkinter

---
+++ reloj_tkinter.py
@@ -0,0 +1,31 @@
+#  Reloj con Tkinter
+
+from tkinter import *
+from time import *
+
+def update():
+    time_string = strftime("%H:%M:%S")
+    time_label.config(text=time_string)
+
+    day_string = strftime("%A")
+    day_label.config(text=day_string)
+
+    date_string = strftime("%B/%d/%Y")
+    date_label.config(text=date_string)
+
+    time_label.after(1000, update)
+
+window = Tk()
+
+time_label = Label(window, font=("Arial",50),fg=("red"),bg="#000")
+time_label.pack()
+
+day_label = Label(window, font=("Ink Free",25))
+day_label.pack()
+
+date_label = Label(window, font=("Ink Free",25))
+date_label.pack()
+
+update()
+
+window.mainloop()
